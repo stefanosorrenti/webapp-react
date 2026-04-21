038df56314f0158d39ac4bd866133dfb80a25381
stefanosorrenti <stefanosorrenti01@gmail.com>
2026-04-21 12:07:35 +0200

    MILESTONE 1: add dependencies of: axios, bootstrap, react-router and bootstrap-icons

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ function App() {
 
   return (
     <>
+      <h1>TEST <i class="bi bi-airplane-fill"></i> </h1>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import * as bootstrap from 'bootstrap'
 import './index.css'
 import App from './App.jsx'
 
