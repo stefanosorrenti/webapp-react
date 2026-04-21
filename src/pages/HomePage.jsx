818cbf250863fcfba870b54d692557948f216f02
stefanosorrenti <stefanosorrenti01@gmail.com>
2026-04-21 14:44:30 +0200

    MILESTONE 3:  add ajax calls in routes to get the respective resources

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,29 @@
+//IMPORTS
+import { useState, useEffect } from "react"
+import axios from "axios"
+
 export default function HomePage() {
 
+    //DATA
+    const [movies, setMovies] = useState([])
 
+    useEffect(() => {
+        axios.get('http://localhost:3000/movies')
+        .then(res => {
+            console.log(res.data);
+            setMovies(res.data)
+        })
+    }, [])
+
+    
+    
     return(
         
-        <h1 className="text-danger">HOMEPAGE</h1>
+        <ul>
+            {movies.map(movie => (
+                <li key={movie.id}>{movie.title}</li>
+            ))}
+        </ul>
+
     )
 }
--- src/pages/MoviePage.jsx
+++ src/pages/MoviePage.jsx
@@ -1,7 +1,10 @@
+//IMPORTS
+import { useParams } from "react-router-dom"
 export default function MoviesPage() {
     
+    const { id } = useParams()
 
     return (
-        <h1 className="text-danger">MOVIE PAGE</h1>
+        <h1 className="text-danger">MOVIE PAGE {id}</h1>
     )
 }
