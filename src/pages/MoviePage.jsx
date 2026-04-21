56694d22f44a61d65176cee2908b35f55921839e
stefanosorrenti <stefanosorrenti01@gmail.com>
2026-04-21 14:59:13 +0200

    fix logic MoviePage

--- src/pages/MoviePage.jsx
+++ src/pages/MoviePage.jsx
@@ -1,10 +1,27 @@
 //IMPORTS
+import axios from "axios"
+import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
-export default function MoviesPage() {
-    
-    const { id } = useParams()
 
+export default function MoviesPage() {
+
+    //DATA
+    const { id } = useParams()
+    const [singleMovie, setSingleMovie] = useState([])
+
+    useEffect(()=> {
+        axios.get(`http://localhost:3000/movies/${id}`)
+    .then(res => {
+
+        setSingleMovie(res.data)
+    })
+    }, [])
+
+    //console.log(singleMovie);
+    if(singleMovie.length === 0) return <h1>Loading...</h1>
     return (
-        <h1 className="text-danger">MOVIE PAGE {id}</h1>
+        <ul>
+            <li>{singleMovie.title}</li>
+        </ul>
     )
 }
